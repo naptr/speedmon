2ff61a01c12bd9c61cf3e241a5910724eb691f52
Krishna Eka Yusuf Putra <krishna.putra332@gmail.com>
2025-02-08 16:29:36 +0700

    Change routine process to update instead of append new data

--- routine/db.go
+++ routine/db.go
@@ -1,90 +1,99 @@
 package main
 
 import (
-	"context"
-	"fmt"
-	"os"
-	"strconv"
+    "context"
+    // "fmt"
+    // "os"
+    // "strconv"
+    "net/url"
 
-	"github.com/jackc/pgx/v5"
-	"github.com/joho/godotenv"
+    "github.com/jackc/pgx/v5"
+    "github.com/joho/godotenv"
 )
 
-type GaugeState struct {
-    id 		string
-    speed	int
-    angle	int
-    seatbelt	bool
-    parking	bool
-}
-
-func TestConnection() {
+func GetConnection() (*pgx.Conn, error) {
     env, err := godotenv.Read()
 
     if err != nil {
 	panic(err)
     }
 
+    password := url.QueryEscape(env["SPEEDMON_DB_PASSWORD"])
     connString := "postgresql://" + env["SPEEDMON_DB_USER"] + ":" +
-		  env["SPEEDMON_DB_PASSWORD"] + "@" + env["SPEEDMON_DB_HOST"] +
-		  ":" + env["SPEEDMON_DB_PORT"] + "/" + env["SPEEDMON_DB_NAME"]
+		  password + "@" + env["SPEEDMON_DB_HOST"] + ":" +
+		  env["SPEEDMON_DB_PORT"] + "/" + env["SPEEDMON_DB_NAME"]
     conn, err := pgx.Connect(context.Background(), connString)
 
     if err != nil {
-	fmt.Fprintf(os.Stderr, "Unable to connect to database %v", err)
-	os.Exit(1)
+	return nil, err
     }
 
-    defer conn.Close(context.Background())
-    
-    insertQuery := "INSERT INTO testing.state (speed, angle, seatbelt, parking) VALUES (@speed, @angle, @seatbelt, @parking)"
-
-    angle, speed := GenerateAngle()
-    seatbelt := GenerateSeatbeltStatus()
-    parking := GenerateParkingStatus()
-
-    _, err = conn.Exec(context.Background(), insertQuery, &pgx.NamedArgs{
-	"speed": speed,
-	"angle": angle,
-	"seatbelt": strconv.Itoa(seatbelt),
-	"parking": strconv.Itoa(parking),
-    })
-
-    if err != nil {
-	fmt.Fprintf(os.Stderr, "an error occured while inserting to testing.state %v\n", err)
-	os.Exit(1)
-    }
-
-    selectQuery := "SELECT id, speed, angle, seatbelt, parking FROM testing.state"
-
-    rows, err := conn.Query(context.Background(), selectQuery)
-
-    if err != nil {
-	fmt.Fprintf(os.Stderr, "cannot querying %v\n", err)
-	os.Exit(1)
-    }
-
-    defer rows.Close()
-
-    var states []GaugeState
-
-    for rows.Next() {
-	var state GaugeState
-	err := rows.Scan(
-	    &state.id,
-	    &state.speed,
-	    &state.angle,
-	    &state.seatbelt,
-	    &state.parking,
-	)
-
-	if err != nil {
-	    fmt.Fprintf(os.Stderr, "err: %v\n", err)
-	    os.Exit(1)
-	}
-
-	states = append(states, state)
-    }
-
-    fmt.Printf("%v\n", states)
+    return conn, nil
 }
+
+// func TestConnection() {
+//
+//     conn, err := GetConnection()
+//
+//     if err != nil {
+// 	fmt.Fprintf(os.Stderr, "unable to connect to database: %v\n", err)
+//     }
+//
+//     var testRes string
+//     err = conn.QueryRow(context.Background(), "SELECT 1+1").Scan(&testRes)
+//
+//     if err != nil {
+// 	fmt.Fprintf(os.Stderr, "an error occured while trying to query to database: %v", err)
+//     }
+//
+//     insertQuery := "INSERT INTO testing.state (speed, angle, seatbelt, parking) VALUES (@speed, @angle, @seatbelt, @parking)"
+//
+//     angle, speed := GenerateAngle()
+//     seatbelt := GenerateSeatbeltStatus()
+//     parking := GenerateParkingStatus()
+//
+//     _, err = conn.Exec(context.Background(), insertQuery, &pgx.NamedArgs{
+// 	"speed": speed,
+// 	"angle": angle,
+// 	"seatbelt": strconv.Itoa(seatbelt),
+// 	"parking": strconv.Itoa(parking),
+//     })
+//
+//     if err != nil {
+// 	fmt.Fprintf(os.Stderr, "an error occured while inserting to testing.state %v\n", err)
+// 	os.Exit(1)
+//     }
+//
+//     selectQuery := "SELECT id, speed, angle, seatbelt, parking FROM testing.state"
+//
+//     rows, err := conn.Query(context.Background(), selectQuery)
+//
+//     if err != nil {
+// 	fmt.Fprintf(os.Stderr, "cannot querying %v\n", err)
+// 	os.Exit(1)
+//     }
+//
+//     defer rows.Close()
+//
+//     var states []GaugeState
+//
+//     for rows.Next() {
+// 	var state GaugeState
+// 	err := rows.Scan(
+// 	    &state.id,
+// 	    &state.speed,
+// 	    &state.angle,
+// 	    &state.seatbelt,
+// 	    &state.parking,
+// 	)
+//
+// 	if err != nil {
+// 	    fmt.Fprintf(os.Stderr, "err: %v\n", err)
+// 	    os.Exit(1)
+// 	}
+//
+// 	states = append(states, state)
+//     }
+//
+//     fmt.Printf("%v\n", states)
+// }
--- routine/main.go
+++ routine/main.go
@@ -1,5 +1,157 @@
 package main
 
+import (
+    "context"
+    "fmt"
+    "os"
+    "strconv"
+
+    "github.com/jackc/pgx/v5"
+)
+
+type Vehicle struct {
+    id 		int8
+    name	string
+}
+
+type VehicleState struct {
+    id 			string
+    vehicle_id		int8	
+    speed		int8
+    angle		int8
+    parking		bool
+    seatbelt		bool
+}
+
+func errorHelper(err error) {
+    if err != nil {
+	fmt.Fprintf(os.Stderr, "an error occured: %v\n", err)
+    }
+}
+
+func GetVehicles() []Vehicle {
+    conn, err := GetConnection()
+    errorHelper(err)
+    defer conn.Close(context.Background())
+
+    rows, err := conn.Query(context.Background(), "SELECT id, name FROM testing.vehicles")
+    errorHelper(err)
+    defer rows.Close()
+
+    var vehicles []Vehicle
+    for rows.Next() {
+	var vehicle Vehicle
+	err := rows.Scan(&vehicle.id, &vehicle.name)
+	errorHelper(err)
+
+	vehicles = append(vehicles, vehicle)
+    }
+
+    return vehicles
+}
+
+func GetVehicleState(id int8) VehicleState {
+    conn, err := GetConnection()
+    errorHelper(err)
+    defer conn.Close(context.Background())
+
+    var vehicleState VehicleState
+    query := `
+    SELECT id, vehicle_id, speed, angle, parking, seatbelt
+    FROM testing.vehicle_states vs
+    WHERE vs.vehicle_id in ($1)`
+    err = conn.QueryRow(context.Background(), query, []int8{ id }).Scan(
+	&vehicleState.id,
+	&vehicleState.vehicle_id,
+	&vehicleState.speed,
+	&vehicleState.angle,
+	&vehicleState.parking,
+	&vehicleState.seatbelt,
+    )
+    errorHelper(err)
+
+    return vehicleState
+}
+
+func CreateVehicleState(id int8) int64 {
+    conn, err := GetConnection()
+    errorHelper(err)
+    defer conn.Close(context.Background())
+
+    query := `
+    INSERT INTO testing.vehicle_states (vehicle_id, speed, angle, parking, seatbelt)
+    VALUES (@vehicle_id, @speed, @angle, @parking, @seatbelt)
+    RETURNING id
+    `
+
+    angle, speed := GenerateAngle()
+    parking := GenerateParkingStatus()
+    seatbelt := GenerateSeatbeltStatus()
+
+    commandTag, err := conn.Exec(context.Background(), query, pgx.NamedArgs{
+	"vehicle_id": id,
+	"speed": speed,
+	"angle": angle,
+	"parking": strconv.Itoa(parking),
+	"seatbelt": strconv.Itoa(seatbelt),
+    })
+
+    errorHelper(err)
+
+    fmt.Printf("inserted rows: %d", commandTag.RowsAffected())
+
+    return commandTag.RowsAffected()
+}
+
+func UpdateVehicleState(id int8) int64 {
+    conn, err := GetConnection()
+    errorHelper(err)
+
+    defer conn.Close(context.Background())
+
+    query := `
+    UPDATE testing.vehicle_states vs
+    SET (speed, angle, parking, seatbelt) = (@speed, @angle, @parking, @seatbelt)
+    WHERE vs.vehicle_id in (@vehicle_id)
+    `
+
+    angle, speed := GenerateAngle()
+    parking := GenerateParkingStatus()
+    seatbelt := GenerateSeatbeltStatus()
+
+    commandTag, err := conn.Exec(context.Background(), query, pgx.NamedArgs{
+	"speed": speed,
+	"angle": angle,
+	"parking": strconv.Itoa(parking),
+	"seatbelt": strconv.Itoa(seatbelt),
+	"vehicle_id": id,
+    })
+    errorHelper(err)
+
+    return commandTag.RowsAffected()
+}
+
+func StartRoutine() {
+    vehicles := GetVehicles()
+
+    affectedRowsChann := make(chan int64)
+    vehicleChann := make(chan VehicleState)
+    for _, vehicle := range(vehicles) {
+	go func() {
+	    vehicleChann <- GetVehicleState(vehicle.id)
+	    affectedRowsChann <- UpdateVehicleState(vehicle.id)
+	}()
+    }
+
+    for i := 0; i < len(vehicles); i++ {
+	vs := <-vehicleChann
+	ar := <-affectedRowsChann
+
+	fmt.Printf("vs %v\n", vs)
+	fmt.Printf("ar %v\n", ar)
+    }
+}
+
 func main() {
-    TestConnection()
+    StartRoutine()
 }
